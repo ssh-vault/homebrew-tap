d29f672728077393f6eeb8015b2bd90d2affe356
nbari <nbari@tequila.io>
2016-10-20 22:56:07 +0200

    version 0.7.0

--- ssh-vault.rb
+++ ssh-vault.rb
@@ -3,6 +3,7 @@ class SshVault < Formula
   homepage "https://ssh-vault.com/"
   url "https://bintray.com/nbari/ssh-vault/download_file?file_path=ssh-vault_0.7.0_darwin_amd64.zip"
   sha256 "2db443fa5544324c8284a55f56e373010387e03f37c91bf68098f32eda23204b"
+  version "0.7.0"
 
   def install
     bin.install "ssh-vault"
