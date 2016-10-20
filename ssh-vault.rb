d478cb91239a7a13a4e134279a933d8a6900ad04
nbari <nbari@tequila.io>
2016-10-20 17:41:34 +0200

    update to version 0.7.0

--- ssh-vault.rb
+++ ssh-vault.rb
@@ -1,8 +1,8 @@
 class SshVault < Formula
   desc "encrypt/decrypt using ssh keys"
   homepage "https://ssh-vault.com/"
-  url "https://bintray.com/nbari/ssh-vault/download_file?file_path=ssh-vault_0.6.0_darwin_amd64.zip"
-  sha256 "e3e4219573649f28871552db353f30dd036d0f2c57dd63eb994d4dde0115e460"
+  url "https://bintray.com/nbari/ssh-vault/download_file?file_path=ssh-vault_0.7.0_darwin_amd64.zip"
+  sha256 "7ced43a72a226889a0d98f25c7bebe316d2997f6d3b3451747f24aabf376914b"
   version "64"
 
   def install
