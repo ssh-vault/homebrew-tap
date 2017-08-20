5f34ec42f4d692c45b26e5dd4db7b51436abc9b1
nbari <nbari@tequila.io>
2017-08-20 12:39:48 +0200

    modified:   ssh-vault.rb

--- ssh-vault.rb
+++ ssh-vault.rb
@@ -23,6 +23,11 @@ class SshVault < Formula
         :revision => "eb71ad9bd329b5ac0fd0148dd99bd62e8be8e035"
   end
 
+  go_resource "golang.org/x/sys" do
+    url "https://go.googlesource.com/sys.git",
+        :revision => "43e60d72a8e2bd92ee98319ba9a384a0e9837c08"
+  end
+
   go_resource "github.com/kr/pty" do
     url "https://github.com/kr/pty.git",
         :revision => "2c10821df3c3cf905230d078702dfbe9404c9b23"
